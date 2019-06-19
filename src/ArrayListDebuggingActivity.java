b0caacc760a45c2bb6d55559721d863bb5ae5703
Firehiwot1 <51675215+Firehiwot1@users.noreply.github.com>
2019-06-19 15:32:04 -0400

    Update ArrayListDebuggingActivity.java

--- src/ArrayListDebuggingActivity.java
+++ src/ArrayListDebuggingActivity.java
@@ -3,7 +3,7 @@ public class ArrayListDebuggingActivity {
     public static void main(String[] args){
 
 // create an array list
-                ArrayList <String>al = new ArrayList<>(); //add data type and <>
+                ArrayList <String>al = new ArrayList<String>(); //add data type and <>
                 System.out.println("Initial size of al: " +
                         al.size());
 // add elements to the array list
